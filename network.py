bc70153ed1528d2390efeec128fdc9ab1adffaae
M-M-Mujtaba <l174144@lhr.nu.edu.pk>
2020-04-10 22:41:37 +0500

    incomplete

--- network.py
+++ network.py
@@ -15,8 +15,8 @@ def typu(len):
 
 data_set = read_excel("iris dataset.xlsx")
 data_set = data_set.sample(frac=1).reset_index(drop=True)
-np_data_set = data_set.iloc[:, 0:4].to_numpy().astype(int)
-np_data_set = np.c_[np.ones((len(np_data_set), 1)).astype(int), np_data_set]
+np_data_set = data_set.iloc[:, 0:4].to_numpy().astype(float)
+np_data_set = np.c_[np.ones((len(np_data_set), 1)).astype(float), np_data_set]
 lables = data_set.iloc[:, 4]
 y_list = [typu(len(lables[i])) for i in range(150)]
 y = np.asarray(y_list)
@@ -29,6 +29,7 @@ training_sample_size = 75
 
 W = np.random.random((1, 5))
 
+
 training_examples = np_data_set[:75, :]
 test_examples = np_data_set[75:, :]
 training_labels = y[:, :75]
@@ -38,19 +39,13 @@ cost = [None] * ((max_iter // 100) + 1)
 m = 75
 cost_index = 0
 # Learning the weightes
+print(np.transpose(training_examples))
 for i in range(max_iter + 1):
-    output = np.matmul(W, np.transpose(training_examples))
-    J = 1 / (2 * m) * np.sum(np.power(training_labels - output, 2))
-    if i % 100 == 0:
-        cost[cost_index] = J
-        cost_index += 1
-
-    W = W +  alpha * np.matmul((training_labels - output), training_examples)
 
 plt.plot(cost, 'c')  # ² 3Lg(N)*N/)4
 # plt.legend([line_up, result_line, line_down], ['Upper Bound 3NLg(N)', ' Result', 'Lower Bound NLg(N)/2'])
 # plt.ylabel('Step Count')
 # plt.xlabel("array sizes")
 plt.show()
-
+print(W)
 print(J)
